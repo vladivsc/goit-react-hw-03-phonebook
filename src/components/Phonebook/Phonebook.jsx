aac6be256b2976dd55ed062c970e312160f47512
Vladyslav Ivashchuk <101827554+vladivsc@users.noreply.github.com>
2023-01-27 13:52:20 +0100

    Initial commit

---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,91 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm';
+
+import contacts from './contacts';
+
+import styles from '../Phonebook/phonebook.module.scss';
+
+class Phonebook extends Component {
+  state = {
+    contacts: [...contacts],
+    filter: '',
+  };
+  
+  addContact = ({name, number}) => {
+    this.setState(prevState => {
+      const { contacts } = prevState;
+
+      if (this.checkDuplicate(name)) {
+        return alert(`${name} is already in contacts`);
+      }
+
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      return { contacts: [newContact, ...contacts] };
+    });
+  };
+
+  checkDuplicate(name) {
+    const normalizeName = name.toLowerCase();
+    const { contacts } = this.state;
+    const contactName = contacts.find(({ name }) => {
+      return name.toLowerCase() === normalizeName;
+    });
+    return Boolean(contactName);
+  }
+
+  removeContact = id => {
+    this.setState(({ contacts }) => {
+      const newContact = contacts.filter(item => item.id !== id);
+
+      return { contacts: newContact };
+    });
+  };
+
+  handleFilter = ({target})=> {
+    this.setState({filter: target.value})
+}
+
+  getFilteredContacts() {
+    const { filter, contacts } = this.state;
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalizeFilter = filter.toLowerCase();
+    const result = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizeFilter)
+    );
+
+    return result;
+  }
+
+  render() {
+    const { addContact, removeContact, handleFilter } = this;
+    const { filter } = this.state;
+    const contacts = this.getFilteredContacts();
+    const isContacts = Boolean(contacts.length)
+
+    return (
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
+
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter handleChange={handleFilter} filter={filter} />
+        <ContactList removeContact={removeContact} contacts={contacts} />
+        {!isContacts && <p>No contacts in your phonebook!</p>}
+      </div>
+    );
+  }
+}
+
+export default Phonebook;
